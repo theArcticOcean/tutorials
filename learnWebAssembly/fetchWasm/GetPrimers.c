7e0d5b4d95cef4fcf693c6bb726c318b79ca637d
theArcticOcean <wei_yang1994@163.com>
2022-03-02 21:24:06 +0800

    Add fetchWasm

---
+++ learnWebAssembly/fetchWasm/GetPrimers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+// export the function.
+#ifdef __EMSCRIPTEN__ 
+#include <emscripten.h>
+EMSCRIPTEN_KEEPALIVE
+#endif
+int compute( int a, int b )
+{
+    int ans = 1;
+    for( int i = 0; i < b; ++i )
+    {
+        ans = ans * a;
+    }
+    return ans;
+}
